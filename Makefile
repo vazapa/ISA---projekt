89e10cac53a0554e457eb29cb75ce3b29d135ad0
vazapa <zapletalvaclav201@gmail.com>
2024-09-21 19:25:47 +0200

    .

--- Makefile
+++ Makefile
@@ -1,11 +1,12 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -std=c11
+LDFLAGS = -lncurses
 TARGET = isa-top
 SRCS = isa-top.c
 OBJS = $(SRCS:.c=.o)
 
 $(TARGET): $(OBJS)
-	$(CC) $(CFLAGS) -o $(TARGET) $(OBJS)
+	$(CC) $(CFLAGS) -o $(TARGET) $(OBJS) $(LDFLAGS)
 
 %.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
--- isa-top.c
+++ isa-top.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ncurses.h>
 
 int main(){
-    printf("test");
+    printf("ncurse verze: %d, %d,%d\n", NCURSES_VERSION_MAJOR,
+                                    NCURSES_VERSION_MINOR,
+                                    NCURSES_VERSION_PATCH);
     return 0;
 }
